9a902057a7af09e9895e8b83d74bf65c1430ad2d
Sinharitik589 <sinharitik18112835@gmail.com>
2020-12-06 14:48:54 +0530

    added app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
+  const [count,setCount] = React.useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>
+        {count}
+      </p>
+      <button onClick={() => { setCount(count+1) }}>Increase</button>
+       <button onClick={()=>{setCount(count-1)}}>Decrease</button>
+
+    </>
   );
 }
 
